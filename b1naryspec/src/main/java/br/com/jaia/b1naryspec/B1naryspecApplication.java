2e332bc2bf3cf62ca7f7a492a568a37a283d9d9a
Marcelo Silva <marcelo.silva070899@gmail.com>
2023-09-12 19:19:51 -0300

    Corpo do Back End

---
+++ b1naryspec/src/main/java/br/com/jaia/b1naryspec/B1naryspecApplication.java
@@ -0,0 +1,13 @@
+package br.com.jaia.b1naryspec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B1naryspecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B1naryspecApplication.class, args);
+	}
+
+}
---
+++ b1naryspec/src/test/java/br/com/jaia/b1naryspec/B1naryspecApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jaia.b1naryspec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B1naryspecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
