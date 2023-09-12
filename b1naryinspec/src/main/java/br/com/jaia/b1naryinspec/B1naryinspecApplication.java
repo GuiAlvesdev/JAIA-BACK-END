e06f2799bab111334393cf3fe8d352dcac8012ee
Marcelo Silva <marcelo.silva070899@gmail.com>
2023-09-12 19:30:45 -0300

    Fix a mistake

--- b1naryinspec/src/main/java/br/com/jaia/b1naryinspec/B1naryinspecApplication.java
+++ b1naryinspec/src/main/java/br/com/jaia/b1naryinspec/B1naryinspecApplication.java
@@ -1,13 +1,13 @@
-package br.com.jaia.b1naryspec;
+package br.com.jaia.b1naryinspec;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class B1naryspecApplication {
+public class B1naryinspecApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(B1naryspecApplication.class, args);
+		SpringApplication.run(B1naryinspecApplication.class, args);
 	}
 
 }
--- b1naryinspec/src/test/java/br/com/jaia/b1naryinspec/B1naryinspecApplicationTests.java
+++ b1naryinspec/src/test/java/br/com/jaia/b1naryinspec/B1naryinspecApplicationTests.java
@@ -1,10 +1,10 @@
-package br.com.jaia.b1naryspec;
+package br.com.jaia.b1naryinspec;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class B1naryspecApplicationTests {
+class B1naryinspecApplicationTests {
 
 	@Test
 	void contextLoads() {
